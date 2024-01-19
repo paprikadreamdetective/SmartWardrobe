f5459db7bdd755e19515717da72ce68b7ce2bcd7
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-01-18 22:45:20 -0600

    simple login is ok

--- web_service/app.py
+++ web_service/app.py
@@ -1,22 +1,10 @@
-from flask import Flask, render_template, request, jsonify, url_for, session
-import pymysql
+from flask import Flask, render_template, request, redirect, jsonify, url_for, session
+from model import ClothesModel
 
 app = Flask(__name__)
-app.secret_key = '12345'
-usuarios = {'usuario1': 'contrasena1', 'usuario2': 'contrasena2'}
-
-# Configuración de la base de datos
+#app.secret_key = '12345'
+#usuarios = {'usuario1': 'contrasena1', 'usuario2': 'contrasena2'}
 '''
-db = pymysql.connect(
-        host = '192.168.100.136',
-        user = 'soulg',
-        password = '11110000',
-        database = 'closet',
-        port = 3306
-    )
-cursor = db.cursor()
-'''
-cnt = 0
 db_config = {
     'host': '192.168.100.136',
     'user': 'soulg',
@@ -91,6 +79,73 @@ def agregar_prenda():
         print(f"Error al agregar prenda: {e}")
         #return render_template('error.html', mensaje='Error al agregar prenda')
     return render_template('exito.html', nombre=nombre)
+'''
+
+db_config = {
+    'host': '192.168.100.136',
+    'user': 'soulg',
+    'password': '11110000',
+    'database': 'closet',
+    'port': 3306
+}
+
+clothes_model = ClothesModel(db_config)
+
+@app.route('/')
+def index():
+    clothes = clothes_model.get_all_clothes()
+    return render_template('index.html', clothes=clothes)
+
+@app.route('/login', methods=['POST'])
+def login():
+    usuario = request.form['campoDatos1']
+    password = request.form['campoDatos2']
+    print(f'c1: {usuario}, c2: {password}')
+    if usuario == "admin" and password == "12345":
+        return ( render_template( 'main_menu.html' ) )
+    else:
+        return ('Credenciales incorrectas. <a href="/">Volver al inicio de sesión</a>')
+
+@app.route('/logout')
+def logout():
+    session.pop('usuario', None)
+    return redirect(url_for('index'))
+
+@app.route('/add_form_page')
+def add_form_page():
+    return render_template('add_form_page.html')
+
+@app.route('/add_form', methods=['POST'])
+def agregar_prenda():
+    nombre = request.form['nombre']
+    tipo = request.form['tipo']
+    color = request.form['color']
+    talla = request.form['talla']
+    cantidad = request.form['cantidad']
+
+    clothes_model.add_clothes(nombre, tipo, color, talla, cantidad)
+    return redirect(url_for('index'))
+
+@app.route('/edit_form_page/<int:cloth_id>')
+def edit_form_page(cloth_id):
+    cloth = clothes_model.get_cloth_by_id(cloth_id)
+    return render_template('edit_form_page.html', cloth=cloth)
+
+@app.route('/edit_form/<int:cloth_id>', methods=['POST'])
+def editar_prenda(cloth_id):
+    nombre = request.form['nombre']
+    tipo = request.form['tipo']
+    color = request.form['color']
+    talla = request.form['talla']
+    cantidad = request.form['cantidad']
+
+    clothes_model.update_cloth(cloth_id, nombre, tipo, color, talla, cantidad)
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:cloth_id>')
+def eliminar_prenda(cloth_id):
+    clothes_model.delete_cloth(cloth_id)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
